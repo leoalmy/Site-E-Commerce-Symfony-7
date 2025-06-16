0bb894a7285f934c1ed2a06b0bd1e6b7aa6dad13
Leo ALMY <leo.almy@edouardgand.fr>
2025-06-16 10:31:59 +0200

    vidéo 8

--- src/Controller/SubCategoryController.php
+++ src/Controller/SubCategoryController.php
@@ -18,7 +18,7 @@ final class SubCategoryController extends AbstractController
     public function index(SubCategoryRepository $subCategoryRepository): Response
     {
         return $this->render('sub_category/index.html.twig', [
-            'sub_categories' => $subCategoryRepository->findAll(),
+            'sub_categories' => $subCategoryRepository->findBy([], ['id' => 'ASC']),
         ]);
     }
 
